92c1e6f0b2f56d380dec5a9f6e41b78251e03c9a
marley010 <9022034@student.zadkine.nl>
2026-04-01 14:17:13 +0200

    navbar

--- Damelief/src/App.jsx
+++ Damelief/src/App.jsx
@@ -3,15 +3,28 @@ import Aanbod from "./pages/Aanbod";
 import OverOns from "./pages/OverOns";
 import Contact from "./pages/Contact";
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import AfspraakMaken from "./pages/AfspraakMaken";
+import "../src/styling/App/App.css"; // we gebruiken deze voor alle globale styling
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path="/aanbod" element={<Aanbod />} />
-      <Route path="/over-ons" element={<OverOns />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
+    <div className="page">
+      <Navbar />
+
+      <main className="page__content">
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/aanbod" element={<Aanbod />} />
+          <Route path="/over-ons" element={<OverOns />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/afspraak-maken" element={<AfspraakMaken />} />
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
   );
 }
 
---
+++ Damelief/src/pages/AfspraakMaken.jsx
@@ -0,0 +1,114 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import 
+function AfspraakMaken() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    phone: "",
+    email: "",
+    date: "",
+    message: "",
+  });
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log("Form submitted");
+    setFormData({
+      firstname: e.target.firstname.value,
+      lastname: e.target.lastname.value,
+      phone: e.target.phone.value,
+      email: e.target.email.value,
+      date: e.target.date.value,
+      message: e.target.message.value,
+    });
+    console.log(formData);
+  };
+  return (
+    <section className="contact">
+      <h1>Afspraak Maken</h1>
+      <form className="contact__form" onSubmit={submitForm}>
+        <div className="contact__form__container-double">
+          <div>
+            <label htmlFor="firstname">Voornaam:</label>
+            <input
+              type="text"
+              id="firstname"
+              name="firstname"
+              value={formData.firstname}
+              onChange={(e) =>
+                setFormData({ ...formData, firstname: e.target.value })
+              }
+            />
+          </div>
+          <div>
+            <label htmlFor="lastname">Achternaam:</label>
+            <input
+              type="text"
+              id="lastname"
+              name="lastname"
+              value={formData.lastname}
+              onChange={(e) =>
+                setFormData({ ...formData, lastname: e.target.value })
+              }
+            />
+          </div>
+        </div>
+        <div className="contact__form__container">
+          <label htmlFor="phone">Telefoonnummer:</label>
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            value={formData.phone}
+            onChange={(e) =>
+              setFormData({ ...formData, phone: e.target.value })
+            }
+          />
+        </div>
+        <div className="contact__form__container">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={(e) =>
+              setFormData({ ...formData, email: e.target.value })
+            }
+          />
+        </div>
+        <div className="contact__form__container">
+          <label htmlFor="date">Datum:</label>
+          <input
+            type="date"
+            id="date"
+            name="date"
+            value={formData.date}
+            onChange={(e) => setFormData({ ...formData, date: e.target.value })}
+          />
+        </div>
+        <div className="contact__form__container">
+          <label htmlFor="message">Bericht:</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={(e) =>
+              setFormData({ ...formData, message: e.target.value })
+            }
+          ></textarea>
+        </div>
+        <button className="contact__form__button" type="submit">
+          Verstuur
+        </button>
+      </form>
+      <button className="contact__button" onClick={() => navigate(-1)}>
+        Back
+      </button>
+    </section>
+  );
+}
+
+export default AfspraakMaken;
