1c09e5178de8399ae71a44a6a9a2d4ede83564bd
marley010 <9022034@student.zadkine.nl>
2026-04-01 11:12:19 +0200

    footer
    v

--- Damelief/src/components/Footer.jsx
+++ Damelief/src/components/Footer.jsx
@@ -1,8 +1,14 @@
+import "../styling/footer/Footer.css"
+
 function Footer() {
   return (
-    <footer className="footer">
-      <p>© 2026 MijnSite</p>
-    </footer>
+<footer className="footer">
+  <p className="subtitle">Klaar om te ontdekken</p>
+  <p className="subtitle">wat jouw lichaam nodig heeft?</p>
+  <p className="p_small">Plan een gratis kennesmakingsgesprek. Geen verplichtigingen - gewoon</p>
+  <p className="p_small">een eerlijk gesprek over waar  jij staat en wat er mogelijk is</p>
+  <button className="footer_button">Plan jouw afspraak</button>
+</footer>
   );
 }
 
