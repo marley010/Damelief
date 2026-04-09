4dbc1cda608e4fd62e93a83b5a54979fad34b491
|  LXUIEJANE. <9023130@student.zadkine.nl>
2026-04-09 21:31:20 +0200

    FIRST SECTION STYLING DONE..

--- Damelief/src/pages/Home.jsx
+++ Damelief/src/pages/Home.jsx
@@ -1,32 +1,48 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+
 function Home() {
   return (
-    <main className="homepageContainer">
+    <main className="homepage">
 
-      <section className="welcomeImage">
-        <h1>DAMELIEF</h1>
-        <p>Lorem ipsum dolor..</p>
-        <p>Ms. Hatice</p>
-      </section>
+      <article className="homepage__header">
+        <section className="homepage__Left">
+          <p>Orthomoleculair therapeut & systemische coach.</p>
+          <p className="sloganText">
+            Jouw lichaam <br />
+            <span>
+              heeft rust nodig,
+            </span>
+            <br />
+            geen druk.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec libero  dolor, iaculis vitae sodales ac, consectetur vitae urna. Lorem ipsum.
+          </p>
+          <div className="buttonRow">
+            <button className="one">Gratis kennismakings gesprek</button>
+            <button className="two">Mijn aanpak</button>
+          </div>
+        </section>
+        <section className="homepage__Right">
+          <div className="headerText">
+            <h1>DAMELIEF</h1>
+            <p>THERAPEUT & COACH</p>
+          </div>
+          <div className="sloganBox">
+            <p className="excp">"Wees lief voor jezelf, dame."</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit. 
+            </p>
+          </div>
+          <div className="filterNames">
 
-      <div className="sloganDiv">
-        <p>'Wees lief voor jezelf, dame.'</p>
-      </div>
+          </div>
+        </section>
+      </article>
 
-      <section className="homepageContent">
-        <article>
-          <h2>WELCOME, VISITOR.</h2>
-          <div className="lineDecor"></div>
-          <p>Lorem ipsum...</p>
-        </article>
-
-        <article className="FAQContainer">
-          <h3>VEELGESTELDE VRAGEN</h3>
-          <ul>
-            <li></li>
-          </ul>
-        </article> 
-      </section>
-
+    
     </main>
   );
 }
