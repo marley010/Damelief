995ccac031dabe955846ceae9217dcd729590aa4
|  LXUIEJANE. <9023130@student.zadkine.nl>
2026-04-16 23:37:51 +0200

    fix on the nav hovers

--- Damelief/src/pages/OverOns.jsx
+++ Damelief/src/pages/OverOns.jsx
@@ -3,18 +3,9 @@ import { useNavigate } from "react-router-dom";
 function OverOns() {
   const navigate = useNavigate();
   return (
-    <div className="page">
-      <div className="page__background-image" />
-
-      <div className="page__fade--top" />
-      <div className="page__fade--bottom" />
-
-      <div className="page__content">
-        <div className="page__text-bubble">
-          <p>Dit is de over-ons pagina.</p>
-        </div>
-      </div>
-    </div>
+    <main className="aboutMe">
+      
+    </main>
   );
 }
 
